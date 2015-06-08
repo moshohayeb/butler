ceb47e632bacf4c518b386e8cde76b72368c7b18
Mohammed Alshohayeb <moshohayeb@gmail.com>
2015-06-08 18:15:49 +0300

    example

--- example/add.js
+++ example/add.js
@@ -1,30 +1,59 @@
-var run = require('./support').run
+var run   = require('./support').run
+var store = require('./store')
+
+var addServer = function (stream, context) {
+  console.log(context)
+  stream.end()
+}
+
 
 module.exports = {
-  name: 'add',
-  help: 'add new components',
+  name:     'add',
+  help:     'add new components',
   commands: [
     {
-      name: 'server',
-      help: 'add a new server',
-      run: run,
+      name:    'server',
+      help:    'add a new server',
+      run:     addServer,
       options: [
         {
-          name: 'ip',
-          help: 'ip address of the server',
+          name:     'hostname',
+          help:     'ip address or hostname of the server',
           required: true,
+          match:    /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/,
+          matchName: '<Hostname>',
+          matchHelp: 'insert hostname'
         },
         {
-          name: 'username',
-          help: 'username to use to access machine',
+          name:     'username',
+          help:     'username to use to access machine',
           required: true
+        },
+        {
+          name: 'tags',
+          help: 'server tag list',
+          multiple: true,
+          match: ['web', 'proxy', 'image', 'stage', 'development', 'database', 'cache']
+        },
+        {
+          name: 'location',
+          help: 'server location',
+          match: {
+            ny: 'New york',
+            am: 'Amesterdam',
+            sf: 'San Fransicso',
+            sg: 'Singapore',
+            ln: 'London',
+            fr: 'Frankfurt'
+          }
+
         }
       ]
     },
 
     {
-      name: 'strategy',
-      help: 'add a deployment strategy',
+      name:     'strategy',
+      help:     'add a deployment strategy',
       required: true,
     }
 
