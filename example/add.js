88926985f77811a261aa7594a41f7ba73c0b9e32
Mohammed Alshohayeb <moshohayeb@gmail.com>
2015-06-10 23:04:46 +0300

    pushed

--- example/add.js
+++ example/add.js
@@ -1,30 +1,42 @@
 var run = require('./support').run
 
 module.exports = {
-  name: 'add',
-  help: 'add new components',
+  name:     'add',
+  help:     'add new components',
   commands: [
     {
-      name: 'server',
-      help: 'add a new server',
-      run: run,
+      name:    'server',
+      help:    'add a new server',
+      run:     run,
       options: [
         {
-          name: 'ip',
-          help: 'ip address of the server',
+          name:     'ip',
+          help:     'ip address of the server',
           required: true,
         },
         {
-          name: 'username',
-          help: 'username to use to access machine',
+          name:     'username',
+          help:     'username to use to access machine',
           required: true
+        },
+        {
+          name:  'color',
+          help:  'server color?!',
+          match: ['red', 'green', 'blue', 'magenta', 'cyan'],
+          group: 'feature'
+        },
+        {
+          name:  'shape',
+          help:  'server shape?!',
+          match: ['circle', 'sphere', 'triangle', 'square', 'box'],
+          group: 'feature'
         }
       ]
     },
 
     {
-      name: 'strategy',
-      help: 'add a deployment strategy',
+      name:     'strategy',
+      help:     'add a deployment strategy',
       required: true,
     }
 
--- example/config.js
+++ example/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  colors: true,
+  colors:        true,
   appendDefault: true,
-  prompt: '<COMPANY-CLI> ',
-  motd:   function (write) {
+  appendGroup:   true,
+  prompt:        '<COMPANY-CLI> ',
+  motd:          function (write) {
     write('Last login: Wed Mar 19 12:33:05 2014 from 10.10.12.20'.green)
   }
 }
